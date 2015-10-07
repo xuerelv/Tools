6ac33a95013d9e60475cb2056eebc83e78986c98
xuehj <xuerenlv@163.com>
2015-10-07 12:05:13 +0800

    xhj_tf_idf

--- w_DataMining/src/tf-idf.py
+++ w_DataMining/src/tf-idf.py
@@ -15,7 +15,7 @@ reload(sys)
 sys.setdefaultencoding('utf8')   
 
 #  读取一个文件，返回 每一行，对应的term,并去除停用词，与空格
-def read_one_file(filename,stop_words_list):
+def read_one_file(filename, stop_words_list):
     line_seg_list = []
     file_r = open('./lily/' + filename, 'r')
     for line in file_r.readlines():
@@ -33,13 +33,13 @@ def read_stop_words():
 
 # 判断一个word是不是全是空格
 def is_only_space(word):
-    for i in xrange(0,len(word)):
+    for i in xrange(0, len(word)):
         if word[i] != ' ':
             return False
     return True
 
 # 判段 list 中是否包含 word
-def is_contain(list,word):
+def is_contain(list, word):
     for wo in list:
         if wo == word:
             return True
@@ -47,7 +47,7 @@ def is_contain(list,word):
 
 
 # 对与一个 file list 中的所有内容，如果存在于 stop_words_list 中，则删除,(为空也删除)
-def remove_stop_words_and_space(line_word_list,stop_words_list):
+def remove_stop_words_and_space(line_word_list, stop_words_list):
     re_list = []
     for wo in line_word_list:
         if  not (is_only_space(wo) or is_contain(stop_words_list, wo)):
@@ -63,7 +63,7 @@ if __name__ == '__main__':
     
     print "第一步，加载文件,并去除停用词"
     for filename in filelist:
-        all_files_map[filename[:-4]] = read_one_file(filename,stop_words_list)
+        all_files_map[filename[:-4]] = read_one_file(filename, stop_words_list)
     
     print "第二步，生成10个文件的词典"
     dic_10_file_idf_map = {}
@@ -74,13 +74,13 @@ if __name__ == '__main__':
             
         for term in set(single_file_term_list):
             if dic_10_file_idf_map.has_key(term):
-                dic_10_file_idf_map[term] +=1
+                dic_10_file_idf_map[term] += 1
             else:
-                dic_10_file_idf_map[term] =1
+                dic_10_file_idf_map[term] = 1
     
     print "第三步，对于整个词典，计算idf"
     for term in dic_10_file_idf_map:
-        dic_10_file_idf_map[term] = log(10.0/(1+float(dic_10_file_idf_map[term])))
+        dic_10_file_idf_map[term] = log(10.0 / (1 + float(dic_10_file_idf_map[term])))
         
         
     print "第四步，对于各个file的term list，统计个数"
@@ -100,22 +100,22 @@ if __name__ == '__main__':
         file_term_sum = 0
         dic_one_file_term_map = all_files_term_count_map[filename]
         
-        ## 计算总term的数量
+        # # 计算总term的数量
         for term_key in dic_one_file_term_map:
             file_term_sum += dic_one_file_term_map[term_key]
         
-        ## 计算tf，并纪录tf最大值，用于归一化
+        # # 计算tf，并纪录tf最大值，用于归一化
         max_tf = 0.0
         for term_key in dic_one_file_term_map:
-            one_tf = float(dic_one_file_term_map[term_key])/float(file_term_sum)
+            one_tf = float(dic_one_file_term_map[term_key]) / float(file_term_sum)
             if one_tf > max_tf:
                 max_tf = one_tf
             dic_one_file_term_map[term_key] = one_tf
         
-        ## 对tf值进行归一化
+        # # 对tf值进行归一化
         for term_key in dic_one_file_term_map:
             one_tf = dic_one_file_term_map[term_key]
-            dic_one_file_term_map[term_key] = 0.5 + (0.5 * one_tf)/max_tf
+            dic_one_file_term_map[term_key] = 0.5 + (0.5 * one_tf) / max_tf
             
             
     print "第六步，计算 tf-idf"
@@ -129,23 +129,24 @@ if __name__ == '__main__':
     print "第七步，写入到文件中"
     if not os.path.exists('result/'):
         os.mkdir('result')
+    li_10_file_dict_list = dic_10_file_idf_map.keys()  # # 词典list
     for filename in all_files_map:
-        file_w = open("./result/"+filename+".txt", 'a')
+        file_w = open("./result/" + filename + ".txt", 'a')
         dic_one_file_term_map = all_files_term_count_map[filename]
         for line_list in  all_files_map[filename]:
             line_term_str = ""
-            for term in dic_10_file_idf_map:
+            for term in li_10_file_dict_list:
                 if is_contain(line_list, term):
                     if len(line_term_str) != 0:
                         if dic_one_file_term_map[term] != 0.0:
-                            line_term_str += " "+str(dic_one_file_term_map[term])[1:-4]
+                            line_term_str += " " + str(dic_one_file_term_map[term])[1:-4]
                         else:
-                            line_term_str += " "+str(0)
+                            line_term_str += " " + str(0)
                     else:
                         line_term_str = str(dic_one_file_term_map[term])
                 else:
                     if len(line_term_str) != 0:
-                        line_term_str += " "+ str(0)
+                        line_term_str += " " + str(0)
                     else:
                         line_term_str = str(0)
             line_term_str += '\n'
